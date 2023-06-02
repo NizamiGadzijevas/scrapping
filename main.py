7c98858951bcfddd7411ac05f65ad37bbef33cac
Nizami <nizami@nizami.lt>
2023-06-02 08:20:47 +0300

    įrašo į csv failą

--- main.py
+++ main.py
@@ -12,23 +12,19 @@ with open("cvbankas.csv", 'w', encoding="UTF-8", newline='') as failas:
     csv_writer = csv.writer(failas)
     csv_writer.writerow(['Pareigos', 'Atlyginimas', 'Įmonė', 'Nuoroda'])
 
-for page in range(1, 1 + 1):
-    url = f"https://www.cvbankas.lt/?page={page}"
-    page = requests.get(url).text
-    soup = BeautifulSoup(page, "html.parser")
-    blokai = soup.find_all('div', class_="list_a_wrapper")
+    for page in range(1, 1 + 1):
+        url = f"https://www.cvbankas.lt/?page={page}"
+        page = requests.get(url).text
+        soup = BeautifulSoup(page, "html.parser")
+        blokai = soup.find_all('div', class_="list_a_wrapper")
 
-    for blokas in blokai:
-        try:
-            atlyginimas = blokas.find('span', class_="salary_amount").text
-            pareigos = blokas.find('h3', class_="list_h3").text.strip()
-            imone = blokas.find('span', class_="dib mt5").text.strip()
-            nuoroda = blokas.find('a', class_="list_a can_visited list_a_has_logo")['href']
-            print(atlyginimas, pareigos, imone)
-    #         print(pareigos)
-    #         print(imone)
-    #         # csv_writer.writerow([pareigos, Datlyginimas, imone])
-    #         csv_writer.writerow(['Pareigos', 'Atlyginimas', 'Imone'])
-    #
-        except:
-            pass
+        for blokas in blokai:
+            try:
+                atlyginimas = blokas.find('span', class_="salary_amount").text
+                pareigos = blokas.find('h3', class_="list_h3").text.strip()
+                imone = blokas.find('span', class_="dib mt5").text.strip()
+                print(atlyginimas, pareigos, imone)
+                csv_writer.writerow([pareigos, atlyginimas, imone])
+        #
+            except:
+                pass
