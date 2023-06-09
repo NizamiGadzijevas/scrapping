1bf2309ff2641cd00d0eda90e5b01a0b614fc639
Nizami <nizami@nizami.lt>
2023-06-09 09:08:58 +0300

    testinis į rankas

--- i_rankas.py
+++ i_rankas.py
@@ -1,13 +1,28 @@
 import pandas as pd
+import numpy as np
 
-#create DataFrame
+
 data = pd.read_csv('cvbankas.csv', encoding="utf-8")
 
-data.loc[(data.APMOKĖJIMO_PERIODIŠKUMAS == '€/val.'),'ATLYGINIMAS_NUO']*=160
-data.loc[(data.APMOKĖJIMO_PERIODIŠKUMAS == '€/val.'),'ATLYGINIMAS_IKI']*=160
-data.to_csv('updated_file.csv', index=False)  # Replace 'updated_file.csv' with the desired file name
-print(data)
+if ATLYGINIMAS_NUO <= 625:
+    NPD = ATLYGINIMAS_NUO
+if ATLYGINIMAS_NUO > 625 or ATLYGINIMAS_NUO <= 840:
+    NPD = 625
+if ATLYGINIMAS_NUO > 840 or ATLYGINIMAS_NUO <= 1926:
+    NPD = 625-0.42*(ATLYGINIMAS_NUO - 840)
+if ATLYGINIMAS_NUO > 1926 or ATLYGINIMAS_NUO <= 2864.22:
+    NPD = 400-0.18*(ATLYGINIMAS_NUO-642)
+else:
+    NPD = 0
+mokesciai = Socialinio_draudimo_mokesciai + NPD + Gyventojų_pajamų_mokestis
+Socialinio_draudimo_mokesciai = ATLYGINIMAS_NUO * 0.0199 + ATLYGINIMAS_NUO * 0.0698
 
-#view DataFrame
-# df['ATLYGINIMAS NUO'] = df['APMOKĖJIMO BŪDAS'].map(lambda x: 'Atlanta' if '€/val.' in x else 'Boston' if 'B' in x else 'testas')
-# print(df)
+Gyventojų_pajamų_mokestis = (ATLYGINIMAS_NUO-NPD)*0.2
+# Gyventojų_pajamų_mokestis = round(ATLYGINIMAS_NUO-NPD)*0.2 , 2))
+
+mokesciai = Socialinio_draudimo_mokesciai + NPD + Gyventojų_pajamų_mokestis
+
+data.loc[(data.APMOKĖJIMO_PERIODIŠKUMAS == 'Neatskaičius mokesčių'),'ATLYGINIMAS_NUO'] -= mokesciai
+data.loc[(data.APMOKĖJIMO_PERIODIŠKUMAS == 'Neatskaičius mokesčių'),'ATLYGINIMAS_IKI'] -= mokesciai
+data.to_csv('updated_file.csv', index=False)
+print(data)
