a766c5063a102f2227692a95b29a119be94083bc
Nizami <nizami@nizami.lt>
2023-06-09 09:28:36 +0300

    testinis į rankas

--- i_rankas.py
+++ i_rankas.py
@@ -1,28 +1,27 @@
 import pandas as pd
-import numpy as np
-
 
 data = pd.read_csv('cvbankas.csv', encoding="utf-8")
 
-if ATLYGINIMAS_NUO <= 625:
-    NPD = ATLYGINIMAS_NUO
-if ATLYGINIMAS_NUO > 625 or ATLYGINIMAS_NUO <= 840:
-    NPD = 625
-if ATLYGINIMAS_NUO > 840 or ATLYGINIMAS_NUO <= 1926:
-    NPD = 625-0.42*(ATLYGINIMAS_NUO - 840)
-if ATLYGINIMAS_NUO > 1926 or ATLYGINIMAS_NUO <= 2864.22:
-    NPD = 400-0.18*(ATLYGINIMAS_NUO-642)
-else:
-    NPD = 0
-mokesciai = Socialinio_draudimo_mokesciai + NPD + Gyventojų_pajamų_mokestis
+ATLYGINIMAS_NUO = data['ATLYGINIMAS_NUO']
 Socialinio_draudimo_mokesciai = ATLYGINIMAS_NUO * 0.0199 + ATLYGINIMAS_NUO * 0.0698
 
-Gyventojų_pajamų_mokestis = (ATLYGINIMAS_NUO-NPD)*0.2
-# Gyventojų_pajamų_mokestis = round(ATLYGINIMAS_NUO-NPD)*0.2 , 2))
+if ATLYGINIMAS_NUO <= 625:
+    NPD = ATLYGINIMAS_NUO
+elif ATLYGINIMAS_NUO <= 840:
+    NPD = 625
+elif ATLYGINIMAS_NUO <= 1926:
+    NPD = 625 - 0.42 * (ATLYGINIMAS_NUO - 840)
+elif ATLYGINIMAS_NUO <= 2864.22:
+    NPD = 400 - 0.18 * (ATLYGINIMAS_NUO - 642)
+else:
+    NPD = 0
 
-mokesciai = Socialinio_draudimo_mokesciai + NPD + Gyventojų_pajamų_mokestis
+Gyventoju_pajamu_mokestis = (ATLYGINIMAS_NUO - NPD) * 0.2
 
-data.loc[(data.APMOKĖJIMO_PERIODIŠKUMAS == 'Neatskaičius mokesčių'),'ATLYGINIMAS_NUO'] -= mokesciai
-data.loc[(data.APMOKĖJIMO_PERIODIŠKUMAS == 'Neatskaičius mokesčių'),'ATLYGINIMAS_IKI'] -= mokesciai
+mokesciai = Socialinio_draudimo_mokesciai + NPD + Gyventoju_pajamu_mokestis
+
+data.loc[data['APMOKEJIMO_PERIODISKUMAS'] == 'Neatskaicius mokescius', 'ATLYGINIMAS_NUO'] -= mokesciai
+data.loc[data['APMOKEJIMO_PERIODISKUMAS'] == 'Neatskaicius mokescius', 'ATLYGINIMAS_IKI'] -= mokesciai
+
 data.to_csv('updated_file.csv', index=False)
 print(data)
