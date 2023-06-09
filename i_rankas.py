55e4462f9676805d7205a0776cb4702f1f677da2
Nizami <nizami@nizami.lt>
2023-06-09 10:58:38 +0300

    atlyginimus paverčia į rankas

--- i_rankas.py
+++ i_rankas.py
@@ -1,28 +1,35 @@
 import pandas as pd
+import numpy as np
 
 data = pd.read_csv('cvbankas.csv', encoding="utf-8")
 
-# ATLYGINIMAS_NUO = 2000
 ATLYGINIMAS_NUO = data['ATLYGINIMAS_NUO']
 Socialinio_draudimo_mokesciai = ATLYGINIMAS_NUO * 0.0199 + ATLYGINIMAS_NUO * 0.0698
 
-if ATLYGINIMAS_NUO <= 625:
-    NPD = ATLYGINIMAS_NUO
-elif ATLYGINIMAS_NUO <= 840:
-    NPD = 625
-elif ATLYGINIMAS_NUO <= 1926:
-    NPD = 625 - 0.42 * (ATLYGINIMAS_NUO - 840)
-elif ATLYGINIMAS_NUO <= 2864.22:
-    NPD = 400 - 0.18 * (ATLYGINIMAS_NUO - 642)
-else:
-    NPD = 0
+NPD = np.where(ATLYGINIMAS_NUO <= 625, ATLYGINIMAS_NUO,
+               np.where(ATLYGINIMAS_NUO <= 840, 625,
+                        np.where(ATLYGINIMAS_NUO <= 1926, 625 - 0.42 * (ATLYGINIMAS_NUO - 840),
+                                 np.where(ATLYGINIMAS_NUO <= 2864.22, 400 - 0.18 * (ATLYGINIMAS_NUO - 642), 0))))
 
-Gyventoju_pajamu_mokestis = (ATLYGINIMAS_NUO - NPD) * 0.2
+Gyventojų_pajamų_mokestis = (ATLYGINIMAS_NUO - NPD) * 0.2
 
-mokesciai = Socialinio_draudimo_mokesciai + NPD + Gyventoju_pajamu_mokestis
+mokesciai = round(Socialinio_draudimo_mokesciai + NPD + Gyventojų_pajamų_mokestis, 2)
 print(mokesciai)
-data.loc[data['APMOKĖJIMO_BŪDAS'] == 'Neatskaicius mokescius', 'ATLYGINIMAS_NUO'] -= mokesciai
-data.loc[data['APMOKĖJIMO_BŪDAS'] == 'Neatskaicius mokescius', 'ATLYGINIMAS_IKI'] -= mokesciai
+data.loc[data['APMOKĖJIMO_BŪDAS'] == 'Neatskaičius mokesčių', 'ATLYGINIMAS_NUO'] -= mokesciai
+
+ATLYGINIMAS_IKI = data['ATLYGINIMAS_IKI']
+Socialinio_draudimo_mokesciai = ATLYGINIMAS_IKI * 0.0199 + ATLYGINIMAS_IKI * 0.0698
+
+NPD = np.where(ATLYGINIMAS_IKI <= 625, ATLYGINIMAS_IKI,
+               np.where(ATLYGINIMAS_IKI <= 840, 625,
+                        np.where(ATLYGINIMAS_IKI <= 1926, 625 - 0.42 * (ATLYGINIMAS_IKI - 840),
+                                 np.where(ATLYGINIMAS_IKI <= 2864.22, 400 - 0.18 * (ATLYGINIMAS_IKI - 642), 0))))
+
+Gyventojų_pajamų_mokestis = (ATLYGINIMAS_IKI - NPD) * 0.2
+
+mokesciai = round(Socialinio_draudimo_mokesciai + NPD + Gyventojų_pajamų_mokestis, 2)
+data.loc[data['APMOKĖJIMO_BŪDAS'] == 'Neatskaičius mokesčių', 'ATLYGINIMAS_IKI'] -= mokesciai
+data.loc[data['APMOKĖJIMO_BŪDAS'] == 'Neatskaičius mokesčių', 'APMOKĖJIMO_BŪDAS'] = "Į rankas"
 
 data.to_csv('updated_file.csv', index=False)
-print(data)
+# print(data)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import pandas as pd
+import numpy as np
 
 
 page = requests.get("https://www.cvbankas.lt/?page=1").text
@@ -55,7 +56,43 @@ with open("cvbankas.csv", 'w', encoding="UTF-8", newline='') as failas:
 
             except:
                 pass
+
 data = pd.read_csv('cvbankas.csv', encoding="utf-8")
-data.loc[(data.APMOKĖJIMO_PERIODIŠKUMAS == '€/val.'),'ATLYGINIMAS_NUO']*=160
-data.loc[(data.APMOKĖJIMO_PERIODIŠKUMAS == '€/val.'),'ATLYGINIMAS_IKI']*=160
-data.to_csv('updated_file.csv', index=False)  # Replace 'updated_file.csv' with the desired file name
+data.loc[data['APMOKĖJIMO_PERIODIŠKUMAS'] == '€/val.','ATLYGINIMAS_NUO']*=160
+data.loc[data['APMOKĖJIMO_PERIODIŠKUMAS'] == '€/val.','ATLYGINIMAS_IKI']*=160
+data.loc[data['APMOKĖJIMO_PERIODIŠKUMAS'] == '€/val.','APMOKĖJIMO_PERIODIŠKUMAS'] = "€/mėn."
+
+data.to_csv('updated_file.csv', index=False)
+
+
+ATLYGINIMAS_NUO = data['ATLYGINIMAS_NUO']
+Socialinio_draudimo_mokesciai = ATLYGINIMAS_NUO * 0.0199 + ATLYGINIMAS_NUO * 0.0698
+
+NPD = np.where(ATLYGINIMAS_NUO <= 625, ATLYGINIMAS_NUO,
+               np.where(ATLYGINIMAS_NUO <= 840, 625,
+                        np.where(ATLYGINIMAS_NUO <= 1926, 625 - 0.42 * (ATLYGINIMAS_NUO - 840),
+                                 np.where(ATLYGINIMAS_NUO <= 2864.22, 400 - 0.18 * (ATLYGINIMAS_NUO - 642), 0))))
+
+Gyventojų_pajamų_mokestis = (ATLYGINIMAS_NUO - NPD) * 0.2
+
+mokesciai = round(Socialinio_draudimo_mokesciai + NPD + Gyventojų_pajamų_mokestis, 2)
+print(mokesciai)
+data.loc[data['APMOKĖJIMO_BŪDAS'] == 'Neatskaičius mokesčių', 'ATLYGINIMAS_NUO'] -= mokesciai
+
+ATLYGINIMAS_IKI = data['ATLYGINIMAS_IKI']
+Socialinio_draudimo_mokesciai = ATLYGINIMAS_IKI * 0.0199 + ATLYGINIMAS_IKI * 0.0698
+
+NPD = np.where(ATLYGINIMAS_IKI <= 625, ATLYGINIMAS_IKI,
+               np.where(ATLYGINIMAS_IKI <= 840, 625,
+                        np.where(ATLYGINIMAS_IKI <= 1926, 625 - 0.42 * (ATLYGINIMAS_IKI - 840),
+                                 np.where(ATLYGINIMAS_IKI <= 2864.22, 400 - 0.18 * (ATLYGINIMAS_IKI - 642), 0))))
+
+Gyventojų_pajamų_mokestis = (ATLYGINIMAS_IKI - NPD) * 0.2
+
+mokesciai = round(Socialinio_draudimo_mokesciai + Gyventojų_pajamų_mokestis, 2)
+data.loc[data['APMOKĖJIMO_BŪDAS'] == 'Neatskaičius mokesčių', 'ATLYGINIMAS_IKI'] -= mokesciai
+data.loc[data['APMOKĖJIMO_BŪDAS'] == 'Neatskaičius mokesčių', 'APMOKĖJIMO_BŪDAS'] = "Į rankas"
+
+data.to_csv('updated_file.csv', index=False)
+# print(data)
+
